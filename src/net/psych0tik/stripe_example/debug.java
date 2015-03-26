e5183fb57802a3ed0637fe00627c99214f6f670e
Richo Healey <richo@psych0tik.net>
2015-03-25 20:42:41 -0700

    Add stub android project

---
+++ src/net/psych0tik/stripe_example/debug.java
@@ -0,0 +1,15 @@
+package net.psych0tik.stripe_example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class debug extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
